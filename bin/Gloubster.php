e7baf5d59c5fd0db7e7fdab3252f1d026ed5bcda
Romain Neutron <imprec@gmail.com>
2012-07-04 17:39:06 +0200

    Rename namespace

--- bin/Gloubster.php
+++ bin/Gloubster.php
@@ -3,11 +3,9 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 
-$Gloubster = require __DIR__ . '/../src/Gloubster/App.php';
-
 $cli = new \Symfony\Component\Console\Application('Gloubster CLI', '0');
 
 $cli->addCommands(array(
-    new Gloubster\Command\GearmanClient('gloubster:gearman:run-client'),
+    new Gloubster\Client\RunClient('gloubster:run-client'),
 ));
 $cli->run();

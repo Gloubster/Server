1d1d770b4f8549bba13615ee783e6d6951d77afe
Romain Neutron <imprec@gmail.com>
2012-12-05 15:29:13 +0100

    Add init script to fetch all dependencies

---
+++ init.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+command -v npm >/dev/null 2>&1  || { echo "I require npm but it's not installed. Aborting." >&2; exit 1; }
+command -v git >/dev/null 2>&1  || { echo "I require git but it's not installed. Aborting." >&2; exit 1; }
+command -v curl >/dev/null 2>&1 || { echo "I require curl but it's not installed. Aborting." >&2; exit 1; }
+command -v php >/dev/null 2>&1  || { echo "I require php but it's not installed. Aborting." >&2; exit 1; }
+command -v bower >/dev/null 2>&1 || { echo "Installing Bower http://twitter.github.com/bower/"; npm install bower -g; }
+command -v bower >/dev/null 2>&1 || { echo "Unable to install bower. Aborting." >&2; exit 1; }
+
+git submodule update --init
+
+if [ !  -f composer.phar ]
+then
+    curl -s http://getcomposer.org/installer | /usr/bin/env php
+fi
+
+/usr/bin/env php composer.phar install

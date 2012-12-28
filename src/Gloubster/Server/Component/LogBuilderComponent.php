20b94a69004cc6ce58307292eaa65fd3bbf2f462
Romain Neutron <imprec@gmail.com>
2012-12-28 17:29:36 +0100

    Add LogBuilderComponent

---
+++ src/Gloubster/Server/Component/LogBuilderComponent.php
@@ -0,0 +1,150 @@
+<?php
+
+namespace Gloubster\Server\Component;
+
+use Gloubster\RabbitMQ\Configuration as RabbitMQConf;
+use Gloubster\Server\GloubsterServer;
+use Gloubster\Message\Factory;
+use Gloubster\Documents\Job;
+use Gloubster\Documents\Garbage;
+use Doctrine\ODM\MongoDB\DocumentManager;
+use Gloubster\Exception\RuntimeException;
+use Gloubster\Message\Job\JobInterface;
+use Monolog\Logger;
+use Predis\Async\Connection\ConnectionInterface as PredisConnection;
+use Predis\Async\Client as PredisClient;
+use React\Promise\Deferred;
+use React\Stomp\AckResolver;
+use React\Stomp\Client;
+use React\Stomp\Protocol\Frame;
+use React\Curry\Util as Curry;
+
+
+class LogBuilderComponent implements ComponentInterface
+{
+
+    /**
+     * {@inheritdoc}
+     */
+    public function register(GloubsterServer $server)
+    {
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function registerSTOMP(GloubsterServer $server, Client $stomp)
+    {
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function registerRedis(GloubsterServer $server, PredisClient $client, PredisConnection $conn)
+    {
+        $server['stomp-client']->subscribeWithAck(
+            sprintf('/queue/%s', RabbitMQConf::QUEUE_LOGS), 'client',
+            Curry::bind(array($this, 'handleLog'), $server['redis'], $server['monolog'])
+        );
+    }
+
+    public function handleLog(PredisClient $redis, Logger $logger, Frame $frame, AckResolver $resolver)
+    {
+        $logger->addInfo(sprintf('Processing job %s', $frame->getHeader('delivery_tag')));
+
+        $error = false;
+
+        try {
+            $job = Factory::fromJson($frame->body);
+            if (!$job instanceof JobInterface) {
+                $error = true;
+            }
+        } catch (RuntimeException $e) {
+            $error = true;
+        }
+
+        if (!$error) {
+            $promise = $this->saveJob($redis, $job);
+        } else {
+            $promise = $this->saveGarbage($redis, $frame->body);
+        }
+
+        $promise->then(function() use ($resolver) {
+            $resolver->ack();
+        });
+
+        return $promise;
+    }
+
+    private function saveJob(PredisClient $redis, JobInterface $job)
+    {
+        $deferred = new Deferred();
+
+        $tx = $redis->multiExec();
+
+        $tx->incr('job-counter');
+        $tx->execute(function ($replies, $redis) use ($deferred, $job) {
+            $hashId = 'job-' . $replies[0];
+
+            $hash = array_merge(array($hashId), $this->hashJob($job), array(function() use ($deferred, $hashId){
+                $deferred->resolve($hashId);
+            }));
+
+            call_user_func_array(array($redis, 'hmset'), $hash);
+        });
+
+        return $deferred->promise()
+          ->then(function ($hashId) use ($redis) {
+                $saddDeferred = new Deferred();
+
+                $redis->sadd('jobs', $hashId, function() use ($hashId, $saddDeferred) {
+                    $saddDeferred->resolve($hashId);
+                });
+
+                return $saddDeferred->promise();
+            });
+    }
+
+    private function saveGarbage(PredisClient $redis, $data)
+    {
+        $deferred = new Deferred();
+
+        $tx = $redis->multiExec();
+
+        $tx->incr('garbage-counter');
+        $tx->execute(function ($replies, $redis) use ($deferred, $data) {
+            $hashId = 'garbage-' . $replies[0];
+
+            $redis->set($hashId, $data, function() use ($deferred, $hashId){
+                $deferred->resolve($hashId);
+            });
+        });
+
+        return $deferred->promise()
+          ->then(function ($hashId) use ($redis) {
+                $saddDeferred = new Deferred();
+
+                $redis->sadd('garbages', $hashId, function() use ($hashId, $saddDeferred) {
+                    $saddDeferred->resolve($hashId);
+                });
+
+                return $saddDeferred->promise();
+            });
+    }
+
+    private function hashJob(JobInterface $job)
+    {
+        $hash = array();
+
+        foreach(json_decode($job->toJson(), true) as $key => $value)
+        {
+            if(is_array($value)) {
+                $value = json_encode($value);
+            }
+            $hash[] = $key;
+            $hash[] = $value;
+        }
+
+        return $hash;
+    }
+}
---
+++ tests/Gloubster/Tests/Server/Component/LogBuilderComponentTest.php
@@ -0,0 +1,265 @@
+<?php
+
+namespace Gloubster\Tests\Server\Component;
+
+use Gloubster\Configuration;
+use Gloubster\Message\Job\ImageJob;
+use Gloubster\Message\Presence\WorkerPresence;
+use Gloubster\Server\GloubsterServer;
+use Gloubster\Server\Component\LogBuilderComponent;
+use Predis\Client as PredisSync;
+use Predis\Async\Client as PredisAsync;
+use React\Stomp\Protocol\Frame;
+use React\EventLoop\Factory as LoopFactory;
+
+class LogBuilderComponentTest extends \PHPUnit_Framework_TestCase
+{
+
+    /** @test */
+    public function itShouldRegister()
+    {
+        $websocket = $this->getMockBuilder('Gloubster\Server\WebsocketApplication')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $client = $this->getMockBuilder('React\Stomp\Client')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $loop = $this->getMockBuilder('React\EventLoop\LoopInterface')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $conf = new Configuration('{
+            "server": {
+                "host": "localhost",
+                "port": 5672,
+                "user": "guest",
+                "password": "guest",
+                "vhost": "/",
+                "server-management": {
+                    "port": 55672,
+                    "scheme": "http"
+                },
+                "stomp-gateway": {
+                    "port": 61613
+                }
+            },
+            "redis-server": {
+                "host": "localhost",
+                "port": 6379
+            },
+            "session-server": {
+                "type": "memcache",
+                "host": "localhost",
+                "port": 11211
+            },
+            "websocket-server": {
+                "address": "local.gloubster",
+                "port": 9990
+            },
+            "listeners": [
+                {
+                    "type": "' . str_replace('\\', '\\\\', __NAMESPACE__) . '\\\\ListenerTester",
+                    "options": {
+                        "transport": "tcp",
+                        "address": "0.0.0.0",
+                        "port": 22345
+                    }
+                }
+            ]
+        }');
+
+        $loop->created = null;
+
+        $server = new GloubsterServer($websocket, $client, $loop, $conf, $this->getLogger());
+        $server->register(new LogBuilderComponent());
+
+        // this attach listeners to the stomp server
+        $server->activateRedisServices(
+            $this->getMockBuilder('Predis\\Async\\Client')
+                ->disableOriginalConstructor()
+                ->getMock(), $this->getMockBuilder('Predis\Async\Connection\ConnectionInterface')
+                ->disableOriginalConstructor()
+                ->getMock()
+        );
+    }
+
+    public function testHandleLogWithJob()
+    {
+        $job = new ImageJob();
+        $job->setBeginning('begin');
+        $job->setEnd('end');
+
+        $frame = new Frame('MESSAGE', array('delivery_tag' => 'delivery-' . mt_rand()), $job->toJson());
+
+        $loop = LoopFactory::create();
+        $options = array(
+            'eventloop' => $loop,
+            'on_error'  => array($this, 'throwRedisError'),
+        );
+
+        $redisSync = new PredisSync('tcp://127.0.0.1:6379');
+        $redisSync->connect();
+
+        $done = false;
+
+        $redis = new PredisAsync('tcp://127.0.0.1:6379', $options);
+        $redis->connect(function() use ($redis, $frame, $redisSync, &$done) {
+            $component = new LogBuilderComponent();
+
+            $resolver = $this->getResolver();
+            $resolver->expects($this->once())
+                ->method('ack');
+
+            $component->handleLog($redis, $this->getLogger(), $frame, $resolver)
+                ->then(function ($hashId) use ($redis, $redisSync, &$done) {
+
+                    $redis->disconnect();
+
+                    $data = $redisSync->hgetall($hashId);
+
+                    $this->assertGreaterThan(0, count($data));
+                    $this->assertEquals('Gloubster\Message\Job\ImageJob', $data['type']);
+
+                    $this->assertTrue($redisSync->sismember('jobs', $hashId));
+
+                    $done = true;
+                });
+        });
+
+        $loop->run();
+
+        $this->assertTrue($done);
+    }
+
+    public function testHandleLogWithWrongJob()
+    {
+        $frame = new Frame('MESSAGE', array('delivery_tag' => 'delivery-' . mt_rand()), '{"hello": "world !"}');
+
+        $loop = LoopFactory::create();
+        $options = array(
+            'eventloop' => $loop,
+            'on_error'  => array($this, 'throwRedisError'),
+        );
+
+        $redisSync = new PredisSync('tcp://127.0.0.1:6379');
+        $redisSync->connect();
+
+        $done = false;
+
+        $redis = new PredisAsync('tcp://127.0.0.1:6379', $options);
+        $redis->connect(function() use ($redis, $frame, $redisSync, &$done) {
+            $component = new LogBuilderComponent();
+
+            $resolver = $this->getResolver();
+            $resolver->expects($this->once())
+                ->method('ack');
+
+            $component->handleLog($redis, $this->getLogger(), $frame, $resolver)
+                ->then(function ($hashId) use ($redis, $redisSync, &$done) {
+
+                    $redis->disconnect();
+
+                    $this->assertEquals('{"hello": "world !"}', $redisSync->get($hashId));
+                    $this->assertTrue($redisSync->sismember('garbages', $hashId));
+
+                    $done = true;
+                });
+        });
+
+        $loop->run();
+
+        $this->assertTrue($done);
+    }
+
+    public function testHandleLogWithGoodMessageNotImplementingJobInterface()
+    {
+        $worker = new WorkerPresence();
+        $worker->setMemory(12345);
+
+        $frame = new Frame('MESSAGE', array('delivery_tag' => 'delivery-' . mt_rand()), $worker->toJson());
+
+        $loop = LoopFactory::create();
+        $options = array(
+            'eventloop' => $loop,
+            'on_error'  => array($this, 'throwRedisError'),
+        );
+
+        $redisSync = new PredisSync('tcp://127.0.0.1:6379');
+        $redisSync->connect();
+
+        $done = false;
+
+        $redis = new PredisAsync('tcp://127.0.0.1:6379', $options);
+        $redis->connect(function() use ($redis, $frame, $redisSync, &$done, $worker) {
+            $component = new LogBuilderComponent();
+
+            $resolver = $this->getResolver();
+            $resolver->expects($this->once())
+                ->method('ack');
+
+            $component->handleLog($redis, $this->getLogger(), $frame, $resolver)
+                ->then(function ($hashId) use ($redis, $redisSync, &$done, $worker) {
+
+                    $redis->disconnect();
+
+                    $this->assertEquals($worker->toJson(), $redisSync->get($hashId));
+                    $this->assertTrue($redisSync->sismember('garbages', $hashId));
+
+                    $done = true;
+                });
+        });
+
+        $loop->run();
+
+        $this->assertTrue($done);
+    }
+
+    public function testRegisterSTOMPServicesProducesNoError()
+    {
+        $server = $this->getMockBuilder('Gloubster\\Server\\GloubsterServer')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $stomp = $this->getMockBuilder('React\\Stomp\\Client')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $component = new LogBuilderComponent();
+        $component->registerSTOMP($server, $stomp);
+    }
+
+    public function testThatRegisterSTOMPDoesNotThrowError()
+    {
+        $server = $this->getMockBuilder('Gloubster\\Server\\GloubsterServer')
+                    ->disableOriginalConstructor()
+                    ->getMock();
+
+        $stomp = $this->getMockBuilder('React\\Stomp\\Client')
+                    ->disableOriginalConstructor()
+                    ->getMock();
+
+        $component = new LogBuilderComponent();
+        $component->registerSTOMP($server, $stomp);
+    }
+
+    private function getLogger()
+    {
+        return $this->getMockBuilder('Monolog\\Logger')
+            ->disableOriginalConstructor()
+            ->getMock();
+    }
+
+    private function getResolver()
+    {
+        return $this->getMockBuilder('React\\Stomp\\AckResolver')
+            ->disableOriginalConstructor()
+            ->getMock();
+    }
+
+    public function throwRedisError($client, $exception, $conn)
+    {
+        throw $exception;
+    }
+}

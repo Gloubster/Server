3ce028fe37ead262063c99ffe6ba298e13cb34c2
Romain Neutron <imprec@gmail.com>
2013-01-07 21:36:27 +0100

    Add post install composer script

--- src/Gloubster/ComposerScripts.php
+++ src/Gloubster/ComposerScripts.php
@@ -4,8 +4,18 @@ namespace Gloubster;
 
 class ComposerScripts
 {
+    public static function postInstallCmd()
+    {
+        echo "Executing post install command";
+        $cwd = getcwd();
+        chdir(__DIR__ . '/../../');
+        system(__DIR__ . "/../../init.sh");
+        chdir($cwd);
+    }
+    
     public static function postPackageInstall()
     {
+        echo "Executing post package install";
         $cwd = getcwd();
         chdir(__DIR__ . '/../../');
         system(__DIR__ . "/../../init.sh");

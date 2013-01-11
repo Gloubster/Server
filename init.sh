9da4ce507f5185b65344cc89e84e50b64d285c66
Romain Neutron <imprec@gmail.com>
2013-01-11 13:30:59 +0100

    Make gloubster and exiftool executables

--- init.sh
+++ init.sh
@@ -7,5 +7,8 @@ command -v php >/dev/null 2>&1  || { echo "I require php but it's not installed.
 command -v bower >/dev/null 2>&1 || { echo "Installing Bower http://twitter.github.com/bower/"; npm install bower -g; }
 command -v bower >/dev/null 2>&1 || { echo "Unable to install bower. Aborting." >&2; exit 1; }
 
+chmod +x bin/glouster
+chmod +x vendor/phpexiftool/exiftool/exiftool
+
 git submodule update --init
 /usr/bin/env bower install
